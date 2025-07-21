3471275f5bc9d16082d921fc4237777525ae851c
jolee <jolee@rbbn.com>
2025-07-21 03:05:34 +0000

    use zhtw

--- main/assets/lang_config.h
+++ main/assets/lang_config.h
@@ -3,59 +3,59 @@
 
 #include <string_view>
 
-#ifndef zh_cn
-    #define zh_cn  // 預設語言
+#ifndef zh_tw
+    #define zh_tw  // 預設語言
 #endif
 
 namespace Lang {
     // 语言元数据
-    constexpr const char* CODE = "zh-CN";
+    constexpr const char* CODE = "zh-TW";
 
     // 字符串资源
     namespace Strings {
-        constexpr const char* ACCESS_VIA_BROWSER = "，浏览器访问 ";
-        constexpr const char* ACTIVATION = "激活设备";
-        constexpr const char* BATTERY_CHARGING = "正在充电";
-        constexpr const char* BATTERY_FULL = "电量已满";
-        constexpr const char* BATTERY_LOW = "电量不足";
-        constexpr const char* BATTERY_NEED_CHARGE = "电量低，请充电";
-        constexpr const char* CHECKING_NEW_VERSION = "检查新版本...";
-        constexpr const char* CHECK_NEW_VERSION_FAILED = "检查新版本失败，将在 %d 秒后重试：%s";
-        constexpr const char* CONNECTED_TO = "已连接 ";
-        constexpr const char* CONNECTING = "连接中...";
-        constexpr const char* CONNECT_TO = "连接 ";
-        constexpr const char* CONNECT_TO_HOTSPOT = "手机连接热点 ";
-        constexpr const char* DETECTING_MODULE = "检测模组...";
-        constexpr const char* ENTERING_WIFI_CONFIG_MODE = "进入配网模式...";
-        constexpr const char* ERROR = "错误";
-        constexpr const char* INFO = "信息";
+        constexpr const char* ACCESS_VIA_BROWSER = "，瀏覽器訪問 ";
+        constexpr const char* ACTIVATION = "啟用設備";
+        constexpr const char* BATTERY_CHARGING = "正在充電";
+        constexpr const char* BATTERY_FULL = "電量已滿";
+        constexpr const char* BATTERY_LOW = "電量不足";
+        constexpr const char* BATTERY_NEED_CHARGE = "電量低，請充電";
+        constexpr const char* CHECKING_NEW_VERSION = "檢查新版本...";
+        constexpr const char* CHECK_NEW_VERSION_FAILED = "檢查新版本失敗，將在 %d 秒後重試：%s";
+        constexpr const char* CONNECTED_TO = "已連接 ";
+        constexpr const char* CONNECTING = "連接中...";
+        constexpr const char* CONNECT_TO = "連接 ";
+        constexpr const char* CONNECT_TO_HOTSPOT = "手機連接WiFi ";
+        constexpr const char* DETECTING_MODULE = "檢測模組...";
+        constexpr const char* ENTERING_WIFI_CONFIG_MODE = "正在設定網路...";
+        constexpr const char* ERROR = "錯誤";
+        constexpr const char* INFO = "資訊";
         constexpr const char* INITIALIZING = "正在初始化...";
-        constexpr const char* LISTENING = "聆听中...";
-        constexpr const char* LOADING_PROTOCOL = "登录服务器...";
+        constexpr const char* LISTENING = "聆聽中...";
+        constexpr const char* LOADING_PROTOCOL = "登入伺服器...";
         constexpr const char* MAX_VOLUME = "最大音量";
-        constexpr const char* MUTED = "已静音";
+        constexpr const char* MUTED = "已靜音";
         constexpr const char* NEW_VERSION = "新版本 ";
-        constexpr const char* OTA_UPGRADE = "OTA 升级";
-        constexpr const char* PIN_ERROR = "请插入 SIM 卡";
-        constexpr const char* REGISTERING_NETWORK = "等待网络...";
-        constexpr const char* REG_ERROR = "无法接入网络，请检查流量卡状态";
-        constexpr const char* RTC_MODE_OFF = "AEC 关闭";
-        constexpr const char* RTC_MODE_ON = "AEC 开启";
-        constexpr const char* SCANNING_WIFI = "扫描 Wi-Fi...";
-        constexpr const char* SERVER_ERROR = "发送失败，请检查网络";
-        constexpr const char* SERVER_NOT_CONNECTED = "无法连接服务，请稍后再试";
-        constexpr const char* SERVER_NOT_FOUND = "正在寻找可用服务";
-        constexpr const char* SERVER_TIMEOUT = "等待响应超时";
-        constexpr const char* SPEAKING = "说话中...";
+        constexpr const char* OTA_UPGRADE = "OTA 升級";
+        constexpr const char* PIN_ERROR = "請插入 SIM 卡";
+        constexpr const char* REGISTERING_NETWORK = "等待網絡...";
+        constexpr const char* REG_ERROR = "無法接入網絡，請檢查網路狀態";
+        constexpr const char* RTC_MODE_OFF = "AEC 關閉";
+        constexpr const char* RTC_MODE_ON = "AEC 開啟";
+        constexpr const char* SCANNING_WIFI = "掃描 Wi-Fi...";
+        constexpr const char* SERVER_ERROR = "發送失敗，請檢查網絡";
+        constexpr const char* SERVER_NOT_CONNECTED = "無法連接服務，請稍後再試";
+        constexpr const char* SERVER_NOT_FOUND = "正在尋找可用服務";
+        constexpr const char* SERVER_TIMEOUT = "等待響應超時";
+        constexpr const char* SPEAKING = "說話中...";
         constexpr const char* STANDBY = "待命";
-        constexpr const char* SWITCH_TO_4G_NETWORK = "切换到 4G...";
-        constexpr const char* SWITCH_TO_WIFI_NETWORK = "切换到 Wi-Fi...";
-        constexpr const char* UPGRADE_FAILED = "升级失败";
-        constexpr const char* UPGRADING = "正在升级系统...";
+        constexpr const char* SWITCH_TO_4G_NETWORK = "切換到 4G...";
+        constexpr const char* SWITCH_TO_WIFI_NETWORK = "切換到 Wi-Fi...";
+        constexpr const char* UPGRADE_FAILED = "升級失敗";
+        constexpr const char* UPGRADING = "正在升級系統...";
         constexpr const char* VERSION = "版本 ";
         constexpr const char* VOLUME = "音量 ";
         constexpr const char* WARNING = "警告";
-        constexpr const char* WIFI_CONFIG_MODE = "配网模式";
+        constexpr const char* WIFI_CONFIG_MODE = "網路設定模式";
     }
 
     // 音效资源
